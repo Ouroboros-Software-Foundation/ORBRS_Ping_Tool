b9b5a62a9fb7ee80aa4dad4c1ab87ba2db810591
gama-BHC <du_yingbin@163.com>
2021-01-05 15:19:15 +0800

    添加项目文件。

---
+++ ORBRS_Ping_Tool/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lbTime = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.pbPoll = New System.Windows.Forms.ToolStripProgressBar()
+        Me.spacelabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.osflabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.numPoll = New System.Windows.Forms.NumericUpDown()
+        Me.chkPoll = New System.Windows.Forms.CheckBox()
+        Me.tbLog = New System.Windows.Forms.TextBox()
+        Me.tmTime = New System.Windows.Forms.Timer(Me.components)
+        Me.tmPoll = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.numPoll, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnStart.Location = New System.Drawing.Point(330, 399)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(322, 34)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "手动检测"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'dgv
+        '
+        Me.dgv.AllowUserToAddRows = False
+        Me.dgv.AllowUserToDeleteRows = False
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.TableLayoutPanel1.SetColumnSpan(Me.dgv, 2)
+        Me.dgv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgv.Location = New System.Drawing.Point(3, 3)
+        Me.dgv.Name = "dgv"
+        Me.dgv.ReadOnly = True
+        Me.dgv.RowTemplate.Height = 23
+        Me.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgv.Size = New System.Drawing.Size(649, 390)
+        Me.dgv.TabIndex = 2
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.TableLayoutPanel1.Controls.Add(Me.StatusStrip1, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.dgv, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnStart, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnOpen, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.tbLog, 2, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(984, 461)
+        Me.TableLayoutPanel1.TabIndex = 3
+        '
+        'StatusStrip1
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.StatusStrip1, 3)
+        Me.StatusStrip1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lbTime, Me.pbPoll, Me.spacelabel, Me.osflabel})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 436)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(984, 25)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lbTime
+        '
+        Me.lbTime.Name = "lbTime"
+        Me.lbTime.Size = New System.Drawing.Size(126, 20)
+        Me.lbTime.Text = "0000-00-00 00:00:00"
+        '
+        'pbPoll
+        '
+        Me.pbPoll.Name = "pbPoll"
+        Me.pbPoll.Size = New System.Drawing.Size(150, 19)
+        Me.pbPoll.Step = 1
+        Me.pbPoll.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        '
+        'spacelabel
+        '
+        Me.spacelabel.Name = "spacelabel"
+        Me.spacelabel.Size = New System.Drawing.Size(421, 20)
+        Me.spacelabel.Spring = True
+        '
+        'osflabel
+        '
+        Me.osflabel.Name = "osflabel"
+        Me.osflabel.Size = New System.Drawing.Size(270, 20)
+        Me.osflabel.Text = "Powered by Ouroboros Software Foundation"
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnOpen.Location = New System.Drawing.Point(3, 399)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(321, 34)
+        Me.btnOpen.TabIndex = 3
+        Me.btnOpen.Text = "打开文件"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.numPoll)
+        Me.Panel1.Controls.Add(Me.chkPoll)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(658, 399)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(323, 34)
+        Me.Panel1.TabIndex = 5
+        '
+        'numPoll
+        '
+        Me.numPoll.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.numPoll.Increment = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.numPoll.Location = New System.Drawing.Point(78, 5)
+        Me.numPoll.Maximum = New Decimal(New Integer() {3600, 0, 0, 0})
+        Me.numPoll.Name = "numPoll"
+        Me.numPoll.Size = New System.Drawing.Size(74, 26)
+        Me.numPoll.TabIndex = 1
+        Me.numPoll.Value = New Decimal(New Integer() {60, 0, 0, 0})
+        '
+        'chkPoll
+        '
+        Me.chkPoll.AutoSize = True
+        Me.chkPoll.Dock = System.Windows.Forms.DockStyle.Left
+        Me.chkPoll.Location = New System.Drawing.Point(0, 0)
+        Me.chkPoll.Name = "chkPoll"
+        Me.chkPoll.Size = New System.Drawing.Size(72, 34)
+        Me.chkPoll.TabIndex = 0
+        Me.chkPoll.Text = "自动刷新"
+        Me.chkPoll.UseVisualStyleBackColor = True
+        '
+        'tbLog
+        '
+        Me.tbLog.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tbLog.Location = New System.Drawing.Point(658, 3)
+        Me.tbLog.Multiline = True
+        Me.tbLog.Name = "tbLog"
+        Me.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbLog.Size = New System.Drawing.Size(323, 390)
+        Me.tbLog.TabIndex = 6
+        '
+        'tmTime
+        '
+        Me.tmTime.Enabled = True
+        Me.tmTime.Interval = 1000
+        '
+        'tmPoll
+        '
+        Me.tmPoll.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(984, 461)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ouroboros' Ping Tool  （本软件由Ouroboros软件基金会免费提供）"
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.numPoll, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents dgv As DataGridView
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lbTime As ToolStripStatusLabel
+    Friend WithEvents tmTime As Timer
+    Friend WithEvents spacelabel As ToolStripStatusLabel
+    Friend WithEvents osflabel As ToolStripStatusLabel
+    Friend WithEvents pbPoll As ToolStripProgressBar
+    Friend WithEvents tmPoll As Timer
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents chkPoll As CheckBox
+    Friend WithEvents numPoll As NumericUpDown
+    Friend WithEvents tbLog As TextBox
+End Class
---
+++ ORBRS_Ping_Tool/Form1.vb
@@ -0,0 +1,140 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim dtAdr As New DataTable
+    Dim busy As Boolean = False
+    Dim swLog As StreamWriter
+
+    Private Sub DataInt()
+        dtAdr = New DataTable
+        dtAdr.Columns.Add("Name", GetType(String))
+        dtAdr.Columns.Add("Adr", GetType(String))
+        dtAdr.Columns.Add("St", GetType(String))
+        dtAdr.Columns.Add("Delay", GetType(String))
+        dtAdr.Columns.Add("ErrTime", GetType(String))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1 Then
+            MessageBox.Show("当前应用程序不允许重复运行多个实例！", "禁止运行", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Application.Exit()
+            Exit Sub
+        End If
+        DataInt()
+        dgv.DataSource = dtAdr
+        dgv.Columns(0).HeaderText = "名称"
+        dgv.Columns(1).HeaderText = "地址"
+        dgv.Columns(2).HeaderText = "状态"
+        dgv.Columns(3).HeaderText = "延迟"
+        dgv.Columns(4).HeaderText = "故障时间"
+        dgv.Columns(4).Width = 200
+        'Call btnOpen_Click(btnOpen, Nothing)
+    End Sub
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        chkPoll.Checked = False
+        busy = True
+        Try
+            Dim ofd As New OpenFileDialog
+            With ofd
+                .Title = "打开文件"
+                .Filter = "CSV文件(*.csv)|*.csv|所有文件(*.*)|*.*"
+                .FilterIndex = 1
+                .InitialDirectory = Application.StartupPath
+            End With
+            If ofd.ShowDialog() = DialogResult.OK Then
+                Dim fp As String = ofd.FileName
+                dtAdr.Clear()
+                Dim sr As New StreamReader(fp, System.Text.Encoding.GetEncoding("GB2312"))
+                sr.ReadLine()
+                Do Until sr.EndOfStream
+                    Dim l As String = sr.ReadLine
+                    Dim dr() As String = l.Split(","c)
+                    dtAdr.Rows.Add(dr)
+                Loop
+                dgv.DataSource = dtAdr
+                Dim logfp As String = fp.Substring(0, fp.LastIndexOf("."c) + 1) & "log"
+                swLog = New StreamWriter(logfp, True, System.Text.Encoding.GetEncoding("GB2312"))
+            Else
+                Exit Sub
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        Finally
+            busy = False
+        End Try
+    End Sub
+
+    Private Sub btnStart_Click(sender As Button, e As EventArgs) Handles btnStart.Click
+        sender.Enabled = False
+        btnOpen.Enabled = False
+        pbPoll.Value = 0
+        busy = True
+        Dim p As New Net.NetworkInformation.Ping
+        For i = 0 To dtAdr.Rows.Count - 1
+            Dim LogT As String = vbNullString
+            Try
+                Dim pr As Net.NetworkInformation.PingReply = p.Send(dtAdr(i)(1), 200)
+                If pr.Status = Net.NetworkInformation.IPStatus.Success Then
+                    dtAdr(i)(3) = $"{pr.RoundtripTime}ms"
+                    If IsDBNull(dtAdr(i)(2)) Then WriteLog(i, "连接成功")
+                    dtAdr(i)(2) = "正常"
+                    If Not IsDBNull(dtAdr(i)(4)) Then '上一次有故障
+                        dtAdr(i)(4) = vbNullString
+                        WriteLog(i, "恢复正常")
+                    End If
+                Else
+                    dtAdr(i)(2) = "故障"
+                    dtAdr(i)(3) = pr.Status
+                    If IsDBNull(dtAdr(i)(4)) Then
+                        dtAdr(i)(4) = Format(Now(), "yyyy-MM-dd HH:mm:ss")
+                        WriteLog(i, "故障")
+                    End If
+                End If
+                dgv.CurrentCell = dgv.Rows(i).Cells(0)
+            Catch ex As Exception
+                dtAdr(i)(2) = "未知故障"
+                dtAdr(i)(3) = ex.Message
+                If IsDBNull(dtAdr(i)(4)) Then
+                    dtAdr(i)(4) = Format(Now(), "yyyy-MM-dd HH:mm:ss")
+                End If
+            Finally
+                dgv.Refresh()
+                busy = False
+            End Try
+        Next
+        sender.Enabled = True
+        btnOpen.Enabled = True
+    End Sub
+
+    Private Sub WriteLog(ByVal idx As Integer, ByVal msg As String)
+        Dim log As String = vbNullString
+        log = $"{Format(Now(), "yyyy-MM-dd HH:mm:ss")},{dtAdr(idx)(1)},{msg},{dtAdr(idx)(3)}"
+        swLog.WriteLine(log)
+        swLog.Flush()
+    End Sub
+
+    Private Sub tmTime_Tick(sender As Object, e As EventArgs) Handles tmTime.Tick
+        lbTime.Text = Now
+    End Sub
+
+    Private Sub chkPoll_CheckedChanged(sender As CheckBox, e As EventArgs) Handles chkPoll.CheckedChanged
+        If numPoll.Value = 0 Then Exit Sub
+        numPoll.Enabled = Not sender.Checked
+        tmPoll.Enabled = sender.Checked
+        pbPoll.Maximum = numPoll.Value
+        pbPoll.Value = 0
+
+    End Sub
+
+    Private Sub tmPoll_Tick(sender As Object, e As EventArgs) Handles tmPoll.Tick
+        If Not busy Then pbPoll.Value += 1
+        If pbPoll.Value >= pbPoll.Maximum Then
+            pbPoll.Value = 0
+            If Not busy Then
+                Call btnStart_Click(btnStart, Nothing)
+            End If
+        End If
+    End Sub
+
+End Class
